0d78f62b9c0e0d5c9687c4c4d28de905517cc33b
Davide <davide.deacetis13@gmail.com>
2022-07-01 18:19:08 +0200

    Completate operazioni sulle collezioni.

--- src/main/webapp/assets/js/collezioni.js
+++ src/main/webapp/assets/js/collezioni.js
@@ -1,21 +1,36 @@
+/*
+* File per le operazioni sulle collezioni
+* @Author: Davide De Acetis
+* @Author: Raluca Mihaela Bujoreanu
+*/
+
+//DICHIARAZIONE SELETTORI
 const collezione_result = $('#collezione-result');
 const collezione_empty = $('#collezione-empty');
+const collezioni_container = $('#collezioni-container');
+const id_collezione_update = $('#updateDiscoCollezione_c');
+const id_disco_update = $('#updateDiscoCollezione_d');
+const id_collezione_add = $('#addDiscoCollezione_c');
+const update_disco_collezione_form = $('#updateDiscoCollezione_form_2')
+const add_disco_collezione_form = $('#addDiscoCollezione_form_2')
+//END DICHIARAZIONE SELETTOR
 
-$(document).ready(function () {
-    collezione_result.hide();
-});
-
-
-//2: Elenco collezioni private di un utente
+/*
+* 2: Elenco collezioni private di un utente
+*/
 function getCollezioniUtente() {
     message("", "");
+    clear();
+    toggleVisibility(collezioni_container);
+
     $.ajax({
         url: "rest/collezioni/all",
         method: "GET",
         success: function (data) {
+            //data - lista di url di collezioni
             collezione_result.children().remove();
             if (data.length > 0) {
-                getCollezioni(data);
+                getCollezioni(data); //ottengo le collezioni e le inserisco nella tabella
                 message("Collezioni caricate.", "success");
             } else {
                 handleError("", "", "", "#collezione", "Non ci sono collezioni.");
@@ -28,16 +43,22 @@ function getCollezioniUtente() {
     });
 }
 
-//3: Elenco collezioni condivise con un utente
+/*
+* 3: Elenco collezioni condivise con un utente
+*/
 function getCollezioniCondivise() {
     message("", "");
+    clear();
+    toggleVisibility(collezioni_container);
+
     $.ajax({
         url: "rest/collezioni/condivise",
         method: "GET",
         success: function (data) {
+            //data - lista di url di collezioni
             collezione_result.children().remove();
             if (data.length > 0) {
-                getCollezioni(data);
+                getCollezioni(data); //ottengo le collezioni e le inserisco nella tabella
                 message("Collezioni condivise caricate.", "success");
             } else {
                 handleError("", "", "", "#collezione", "Non ci sono collezioni condivise.");
@@ -50,18 +71,24 @@ function getCollezioniCondivise() {
     });
 }
 
-//4: Singola collezione
+/*
+* 4: Singola collezione
+* @param {int} val - id della collezione da visualizzare
+*/
 function getCollezione(val) {
     message("", "");
-    console.log(val);
+    clear();
+    toggleVisibility(collezioni_container);
+
     if (val) {
         $.ajax({
             url: "rest/collezioni/" + val,
             method: "GET",
             success: function (data) {
+                //data - collezione
                 collezione_result.children().remove();
                 message("Collezione caricata.", "success");
-                populateCollezione(data);
+                populateCollezione(data); //popolo la tabella con la collezione
             },
             error: function (request, status, error) {
                 handleError(request, status, error, "#collezione", "Errore nel caricamento della collezione.");
@@ -73,26 +100,29 @@ function getCollezione(val) {
     }
 }
 
-//10. Aggiungi disco esistente in una collezione
-function updateDiscoCollezione(c, d) {
+/*
+* 6. Crea e aggiunge un disco esistente in una collezione
+* @param {int} c - id della collezione
+* @param {int} d - id del disco nella collezione
+*/
+function addDiscoCollezione(c) {
     message("", "");
-    if (c && d) {
+    if (c) {
         $.ajax({
-            url: "rest/collezioni/" + c + "/dischi/" + d,
+            url: "rest/collezioni/" + c + "/dischi",
             method: "PUT",
             contentType: "application/json",
             data: JSON.stringify({
-                id: d,
-                titolo: $('#titolo').val(),
-                anno: $('#anno').val(),
-                genere: $('#genere').val(),
-                formato: $('#formato').val(),
-                stato_conservazione: $('#statoConservazione').val(),
-                barcode: $('#barcode').val(),
-                etichetta: $('#etichetta').val(),
-                autori: $('#autore').val()
+                titolo: $('#titolo_add').val(),
+                anno: $('#anno_add').val(),
+                genere: $('#genere_add').val(),
+                formato: $('#formato_add').val(),
+                stato_conservazione: $('#statoConservazione_add').val(),
+                barcode: $('#barcode_add').val(),
+                etichetta: $('#etichetta_add').val(),
+                autori: $('#autore_add').val()
             }),
-            success: function (data) {
+            success: function () {
                 collezione_result.children().remove();
                 message("Collezione aggiornata con il nuovo disco.", "success");
             },
@@ -102,11 +132,152 @@ function updateDiscoCollezione(c, d) {
             cache: false,
         });
     } else {
+        clear();
         handleError("", "", "", "#collezione", "Input errato");
     }
 }
 
-//Funzioni di utility
+/*
+* 10. Aggiorna un disco all'interno di una collezione
+* @param {int} c - id della collezione
+* @param {int} d - id del disco nella collezione
+*/
+function updateDiscoCollezione(c, d) {
+    message("", "");
+    if (c && d) {
+        $.ajax({
+            url: "rest/collezioni/" + c + "/dischi/" + d,
+            method: "PUT",
+            contentType: "application/json",
+            //vengono passati i dati della form update_disco_collezione_form
+            data: JSON.stringify({
+                id: d,
+                titolo: $('#titolo_update').val(),
+                anno: $('#anno_update').val(),
+                genere: $('#genere_update').val(),
+                formato: $('#formato_update').val(),
+                stato_conservazione: $('#statoConservazione_update').val(),
+                barcode: $('#barcode_update').val(),
+                etichetta: $('#etichetta_update').val(),
+                autori: $('#autore_update').val()
+            }),
+            success: function () {
+                collezione_result.children().remove();
+                message("Collezione aggiornata con il nuovo disco.", "success");
+            },
+            error: function (request, status, error) {
+                handleError(request, status, error, "#collezione", "Errore nel caricamento della collezione.");
+            },
+            cache: false,
+        });
+    } else {
+        clear();
+        handleError("", "", "", "#collezione", "Input errato");
+    }
+}
+
+/*----------------------------------FUNZIONI UTILITY PER LE COLLEZIONI------------------------------------------------*/
+
+/*
+* Utility che permette di visualizzare le form per le operazioni di add e update di un disco in una collezione
+* @param {String} op - operazione da eseguire (add o update)
+*/
+function discoForm(op) {
+    message("", "");
+    clear();
+    if (op === 'add') { //ADD DI UN DISCO IN UNA COLLEZIONE
+        //Se i campi non sono compilati non viene mostrata la form e si esce dalla funzione.
+        if (id_collezione_add.val() === '') {
+            message('Inserire tutti i campi obbligatori.', 'error')
+            return;
+        }
+        //Mostra la form per creare e aggiungere un disco alla collezione specificata
+        toggleVisibility(add_disco_collezione_form);
+        //Ottiene tutti gli autori del sistema e li inserisce nella select degli autori
+        getAutoriForm($('#autore_add'));
+
+    } else if (op === 'update') { //UPDATE DEL DISCO IN UNA COLLEZIONE
+        if (id_collezione_update.val() === '' || id_disco_update.val() === '') {
+            message('Inserire tutti i campi obbligatori.', 'error')
+            return;
+        }
+        //Mostra la form per aggiornare un disco della collezione specificata
+        toggleVisibility(update_disco_collezione_form);
+        //Popola i campi della form con i dati del disco selezionati
+        populateDiscoCollezioneUpdateForm();
+    }
+}
+
+/*
+* Funzione Utility per Ottiene tutti gli autori del sistema e li inserisce nella select degli autori
+* @param {Selectpicker} select - La select che deve essere riempita con gli autori
+*/
+function getAutoriForm(select) {
+    $.ajax({
+        url: "rest/autori",
+        method: "GET",
+        success: function (data) {
+            $.each(data, function (key) {
+                $.ajax({
+                    url: "rest/autori/" + data[key].split("/")[6],
+                    method: "GET",
+                    success: function (data) {
+                        select.append('<option value="' + data['id'] + '">' + data['nome_artistico'] + '</option>');
+                    }
+                })
+            })
+        },
+        error: function (request, status, error) {
+            handleError(request, status, error, "#collezione", "Errore nel caricamento degli autori.");
+        },
+        cache: false,
+    });
+}
+
+/*
+* Funzione Utility per popolare i campi della form per l'aggiornamento di un disco di una collezione
+*/
+function populateDiscoCollezioneUpdateForm() {
+    const titolo = $('#titolo_update');
+    const anno = $('#anno_update');
+    const genere = $('#genere_update');
+    const formato = $('#formato_update');
+    const statoConservazione = $('#statoConservazione_update');
+    const barcode = $('#barcode_update');
+    const etichetta = $('#etichetta_update');
+    const autori = $('#autore_update');
+
+    $.ajax({
+        url: "rest/dischi/" + id_disco_update.val(),
+        method: "GET",
+        success: function (data) {
+            titolo.val(data['titolo']);
+            anno.val(data['anno']);
+            barcode.val(data['barcode']);
+            etichetta.val(data['etichetta']);
+            genere.val(data['genere']).trigger('change');
+            formato.val(data['formato']).trigger('change');
+            if (formato.val() === 'DIGITALE') {
+                disableStatoConservazione(statoConservazione, formato);
+            } else {
+                statoConservazione.val(data['stato_conservazione']).trigger('change');
+            }
+            getAutoriForm(autori);
+            select_picker.selectpicker('refresh');
+        },
+        error: function (request, status, error) {
+            clear()
+            handleError(request, status, error, "#collezione", "Errore nel caricamento del disco.");
+        },
+        cache: false,
+    });
+}
+
+
+/*
+* Funzione Utility per Ottenere le collezioni
+* @param {List<URL>} data - Lista di URL delle collezioni
+*/
 function getCollezioni(data) {
     $.each(data, function (key) {
         $.ajax({
@@ -123,12 +294,16 @@ function getCollezioni(data) {
     })
 }
 
-
+/*
+* Funzione Utility il riempimento della tabella delle collezioni
+* @param {Collezione} data - Collezione da inserire nella tabella
+*/
 function populateCollezione(data) {
+
     if (data) {
+        //Se la collezione non è vuota viene inserita nella tabella.
         collezione_result.show();
         collezione_empty.hide();
-        collezione_empty.text("Non ci sono collezioni.");
 
         collezione_result.append('<tr>')
         collezione_result.append('<td>' + data['id'] + '</td>')
@@ -137,102 +312,42 @@ function populateCollezione(data) {
         collezione_result.append('<td>' + data['privacy'] + '</td>')
         collezione_result.append('</tr>')
     } else {
+        //Se la collezione è vuota viene svuotata la tabella e viene mostrato un messaggio.
         collezione_result.children().remove();
         collezione_empty.show();
-        collezione_empty.text("Non ci sono collezioni.");
     }
+    collezione_empty.text("Non ci sono collezioni.");
 }
 
-function discoForm(id_disco) {
-    message("", "");
-    toggleVisibility($('#collezioni-container'));
-    if (id_disco === undefined) {
-        toggleVisibility($('#addDiscoCollezione_form_2'));
-        $.ajax({
-            url: "rest/autori",
-            method: "GET",
-            success: function (data) {
-                $.each(data, function (key) {
-                        $.ajax({
-                            url: "rest/autori/" + data[key].split("/")[6],
-                            method: "GET",
-                            success: function (data) {
-                                $('#autore').append('<option value="' + data['id'] + '">' + data['nome_artistico'] + '</option>');
-                            }
-                        })
-                    }
-                )
-            },
-            error: function (request, status, error) {
-                handleError(request, status, error, "#collezione", "Errore nel caricamento degli autori.");
-            },
-            cache: false,
-        });
+/*
+* Alla selezione di un formato nella form update_disco_collezione_form viene disabilitato il campo statoConservazione_update se il formato è digitale
+*/
+$('#formato_update').on('change', function () {
+    disableStatoConservazione($('#statoConservazione_update'), $('#formato_update option:selected'));
+});
+
+/*
+* Alla selezione di un formato nella form add_disco_collezione_form viene disabilitato il campo statoConservazione_add se il formato è digitale
+*/
+$('#formato_add').on('change', function () {
+    disableStatoConservazione($('#statoConservazione_add'), $('#formato_add option:selected'));
+});
+
+/*
+* Utility che disabilita il campo statoConservazione se il formato è digitale
+*/
+function disableStatoConservazione(statoConservazione, selected) {
+    if (selected.val() === 'DIGITALE') {
+        statoConservazione.val('').trigger('change');
+        statoConservazione.prop('disabled', true);
+        statoConservazione.prop('required', false);
+        statoConservazione.parent().addClass('off');
     } else {
-        toggleVisibility($('#updateDiscoCollezione_form_2'));
-        let disco;
-        $.ajax({
-            url: "rest/dischi/" + id_disco,
-            method: "GET",
-            success: function (data) {
-                $('#titolo').val(data['titolo']);
-                $('#anno').val(data['anno']);
-                $('#barcode').val(data['barcode']);
-                $('#etichetta').val(data['etichetta']);
-                $('#genere').val(data['genere']).trigger('change');
-                $('#formato').val(data['formato']).trigger('change');
-                if ($('#formato').val() === 'DIGITALE') {
-                    disableStatoConservazione();
-                } else {
-                    $('#statoConservazione').val(data['stato_conservazione']).trigger('change');
-                }
-                $('.selectpicker').selectpicker('refresh');
-            },
-            error: function (request, status, error) {
-                handleError(request, status, error, "#collezione", "Errore nel caricamento degli autori.");
-            },
-            cache: false,
-        });
-        $.ajax({
-            url: "rest/autori",
-            method: "GET",
-            success: function (data) {
-                $.each(data, function (key) {
-                        $.ajax({
-                            url: "rest/autori/" + data[key].split("/")[6],
-                            method: "GET",
-                            success: function (data) {
-                                $('#autore').append('<option value="' + data['id'] + '">' + data['nome_artistico'] + '</option>');
-                                $('.selectpicker').selectpicker('refresh');
-                            }
-                        })
-                    }
-                );
-            },
-            error: function (request, status, error) {
-                handleError(request, status, error, "#collezione", "Errore nel caricamento degli autori.");
-            },
-            cache: false,
-        });
+        statoConservazione.prop('disabled', false);
+        statoConservazione.prop('required', true);
+        statoConservazione.parent().removeClass('off');
     }
-
-    $('#formato').on('change', function () {
-        disableStatoConservazione();
-    });
-
-    function disableStatoConservazione() {
-        let selected = $('#formato option:selected');
-        let statoConservazione = $('#statoConservazione');
-        if (selected.val() === 'DIGITALE') {
-            statoConservazione.val('').trigger('change');
-            statoConservazione.prop('disabled', true);
-            statoConservazione.prop('required', false);
-            statoConservazione.parent().parent().addClass('off');
-        } else {
-            statoConservazione.prop('disabled', false);
-            statoConservazione.prop('required', true);
-            statoConservazione.parent().parent().removeClass('off');
-        }
-        $('.selectpicker').selectpicker('refresh');
-    }
+    select_picker.selectpicker('refresh');
 }
+
+
--- src/main/webapp/assets/js/utility.js
+++ src/main/webapp/assets/js/utility.js
@@ -1,3 +1,22 @@
+/*
+* File di utility per tutte el operazioni.
+* @Author: Davide De Acetis
+* @Author: Raluca Mihaela Bujoreanu
+*/
+
+const select_picker = $('.selectpicker');
+
+/*
+* Al caricamento della pagina nasconde tutte le tabelle e le form
+*/
+$(document).ready(function () {
+    clear();
+});
+
+/*
+* Utility che mostra un elemento se è nascosto o lo nasconde se è visibile
+* @Param {Elem} elem - Elemento che si vuole nascondere o mostrare.
+*/
 function toggleVisibility(elem) {
     if (elem.css('display') === 'none') {
         elem.css('display', 'block');
@@ -6,6 +25,11 @@ function toggleVisibility(elem) {
     }
 }
 
+/*
+* Utility che imposta un messaggio di errore o di successo dopo la chiamata di un'operazione.
+* @Param {String} message - Messaggio da mostrare.
+* @Param {String} type - Tipo di messaggio, può essere error, success o "".
+*/
 function message (message, type) {
     const message_span = $('#message');
     message_span.html(message);
@@ -21,23 +45,44 @@ function message (message, type) {
     }
 }
 
+/*
+* Utility per la gestione degli errori
+* @Param {String} request - Stringa della richiesta contenente lo status html.
+* @Param {String} status
+* @Param {String} error
+* @Param {String} table - Tabella dell'operazione.
+* @Param {String} msg - Messaggio da inoltrare alla funzione message().
+*/
 function handleError(request, status, error, table, msg) {
-    const table_empty = $(table + "-empty");
-    const table_result = $(table + "-result");
+    const table_empty = $(table + "-empty"); //selettore per la barra empty da mostrare e su cui inserire il messaggio
+    const table_result = $(table + "-result"); //selettore per l'interno della tabella da svuotare.
 
     table_empty.show()
     table_result.children().remove();
     switch (request.status) {
-        case 404:
+        case 404:   //Elemento non trovato
             table_empty.text("Elemento non presente.");
             message("Elemento non presente.", "error");
             break;
-        case 401:
+        case 401:   //Non autorizzato
             table_empty.text("Non sei autorizzato.");
             message("Non sei autorizzato.", "error");
             break;
-        default:
+        case 500:   //Server error
+            table_empty.text("Errore del server.");
+            message("Errore del server.", "error");
+            break;
+        default:    //Qualsiasi altro messaggio che non sia un errore html
             table_empty.text(msg);
             message(msg, "error");
     }
+}
+
+/*
+* Utility rimuove tutte le tabelle e le form sullo schermo.
+*/
+function clear() {
+    collezioni_container.css('display', 'none');
+    update_disco_collezione_form.css('display', 'none');
+    add_disco_collezione_form.css('display', 'none')
 }
